ed7bd1500102cad2db5f71128675c1eaedb30a70
duffyevan <eduffy2015@spyponders.com>
2017-08-03 09:21:44 -0400

    Substitued VMware's Clarity UI in place of Bootstrap and rewrote index accordingly. Looks nice. Will make more fancy in the future

--- index.php
+++ index.php
@@ -1,36 +1,48 @@
 <html>
 <head>
 <title>Test Blog Running Off Converted MD Files</title>
-<!-- BOOTSTRAP HERE -->
-<script
-  src="http://code.jquery.com/jquery-3.2.1.min.js"
-    integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-      crossorigin="anonymous"></script>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="node_modules/clarity-ui/clarity-ui.min.css">
+<link rel="stylesheet" href="node_modules/clarity-icons/clarity-icons.min.css">
 <link rel="stylesheet" href="blog.css">
 </head>
 <body>
-<div class="container">
-<?php
-	$post_seperator = "";
-	$folder = getcwd() . "/posts"; 
-	$files = scandir($folder);
-	foreach(array_reverse($files) as $file){
-		if ($file != "." && $file != ".." && $file != ".htaccess"){
-			echo '<div class="well">';
-			echo "<small>" . pathinfo($file)["filename"] . "</small>";
-			echo file_get_contents($folder . "/" . $file);
-			echo "$post_seperator";
-			echo "</div>";
-		}
-	}
-?>
-<center>
-	<small>Site Created By Evan Duffy</small>
-	<br>
-	<small>August 2017</small>
-</center>
+<div class="main-container">
+<header class="header-6">
+	    <div class="branding">
+	        <a href="#" class="nav-link">
+	            <img src="https://www.evanduffy.org/resources/Bidoof.gif" style="vertical-align: top;"></img>
+	            <span class="title">blog.evanduffy.org</span>
+	        </a>
+	    </div>
+	    <div class="header-nav">
+	    	<a class="nav-link nav-text" href="https://www.evanduffy.org">Home</a>
+    		<a class="active nav-link nav-text" href="#">Blog</a>
+		</div>
+	</header>
+	<div class="content-container">
+		<div class="content-area">
+			<?php
+				$post_seperator = "";
+				$folder = getcwd() . "/posts"; 
+				$files = scandir($folder);
+				foreach(array_reverse($files) as $file){
+					if ($file != "." && $file != ".." && $file != ".htaccess"){
+						echo "<div class='card'>";
+						echo "<div class='card-block'>";
+						echo "<small>" . pathinfo($file)["filename"] . "</small>";
+						echo file_get_contents($folder . "/" . $file);
+						echo "$post_seperator";
+						echo "</div></div>";
+					}
+				}
+			?>
+			<center>
+				<small>Site Created By Evan Duffy</small>
+				<br>
+				<small>August 2017</small>
+			</center>
+		</div>
+	</div>
+
+
 </div>
